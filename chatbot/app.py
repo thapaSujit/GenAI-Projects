935304c36314eb1683d9e5d917b0e03a3a8c6f60
Sujit Thapa <megasuziet@gmail.com>
2024-04-15 12:42:23 +0200

    added

---
+++ chatbot/app.py
@@ -0,0 +1,12 @@
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
