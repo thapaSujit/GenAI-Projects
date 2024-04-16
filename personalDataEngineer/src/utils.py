636281887f6aee0c7d4a5d018f228205cb9e775d
Sujit Thapa <megasuziet@gmail.com>
2024-04-16 15:53:01 +0200

    updated

---
+++ personalDataEngineer/src/utils.py
@@ -0,0 +1,36 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+def load_pdf(file_path):
+    """
+    Load a PDF file using PyPDFLoader.
+
+    Parameters:
+        file_path (str): The path to the PDF file.
+
+    Returns:
+        list: List of loaded documents.
+    """
+    loader = PyPDFLoader(file_path)
+    return loader.load()
+
+# Example usage:
+#pdf_file_path = 'attention.pdf'
+#documents = load_pdf(pdf_file_path)
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def split_documents(inputFile):
+    """
+    Split documents using RecursiveCharacterTextSplitter.
+
+    Parameters:
+        docs (list): List of documents to be split.
+
+    Returns:
+        list: List of split documents.
+    """
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    return text_splitter.split_documents(inputFile)
+
+# Example usage:
+#split_documents_result = split_documents(inputFiles)
