7a2a77f1fb3ae78aabcac89087947e381f17b34c
Sujit Thapa <megasuziet@gmail.com>
2024-04-16 15:35:19 +0200

    added

---
+++ personalDataEngineer/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'personalDataEngineer',
+    version= '0.0.0',
+    author= 'Sujit Thapa',
+    author_email= 'megasuziet@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
