25d0b17f7973864475c232ffa6e71ef9b1270ab4
Sujit Thapa <megasuziet@gmail.com>
2024-04-16 11:33:07 +0200

    updated

--- chatbot/api/api.py
+++ chatbot/api/api.py
@@ -8,22 +8,17 @@ app=FastAPI(
     title="Langchain Server",
     version="1.0",
     decsription="A simple API Server"
-
 )
 
 llm=Ollama(model="llama2")
 
 prompt=ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
 
-
 add_routes(
     app,
     prompt|llm,
     path="/poem"
-
-
 )
-
 
 if __name__=="__main__":
     uvicorn.run(app,host="localhost",port=8002)
