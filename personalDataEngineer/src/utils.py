80aa4192dd6cdd2686ba2c21375206573e9de9a7
Sujit Thapa <megasuziet@gmail.com>
2024-04-16 15:56:06 +0200

    updated

--- personalDataEngineer/src/utils.py
+++ personalDataEngineer/src/utils.py
@@ -1,5 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
 def load_pdf(file_path):
     """
     Load a PDF file using PyPDFLoader.
@@ -34,3 +37,21 @@ def split_documents(inputFile):
 
 # Example usage:
 #split_documents_result = split_documents(inputFiles)
+
+
+def create_faiss_vector_db(documents):
+    """
+    Create a FAISS vector database from documents using OpenAI embeddings.
+
+    Parameters:
+        documents (list): List of documents.
+
+    Returns:
+        FAISS: FAISS vector database.
+    """
+    embeddings = OpenAIEmbeddings()
+    return FAISS.from_documents(documents, embeddings)
+
+# Example usage:
+#faiss_vector_db = create_faiss_vector_db(documents)
+
