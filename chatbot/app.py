0c5499229c0350d2ea8d81fafe9ec3a1f8d16888
Sujit Thapa <megasuziet@gmail.com>
2024-04-16 15:35:27 +0200

    updated

--- chatbot/app.py
+++ chatbot/app.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
